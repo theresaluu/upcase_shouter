fe3a1de373aac0e0f637460a4898c8c7b920660a
Theresa Luu <theresa.luu@gmail.com>
2016-02-17 20:40:17 -0800

    Shouters can shout
    Step 1 of 5 of the Intermediate RoR tutorial on Upcase completed.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'homes#show', via: :get
+
+  resource :dashboard, only: [:show]
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  resources :shouts, only: [:create, :show]
+end
