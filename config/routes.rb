ba4ebec01a6238f0bfee6e2f40ac13d8d5402647
Theresa Luu <theresa.luu@gmail.com>
2016-02-22 23:02:33 -0800

    Complex Associations
    Users can now follow each other without it being a relflexive
    relationship

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    post 'follow' => 'following_relationships#create'
+  end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
